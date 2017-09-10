355d2be60cf06b9d438f9ad5d3d73f2c02542863
way <1320438999@qq.com>
2017-09-10 11:46:39 +0800

    dagger2最简单的例子

---
+++ app/src/main/java/com/ape/dagger2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ape.dagger2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Inject
+    Tinno mTinno;//加入注解，标注这个Tinno是需要注入的
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView dagger2TextView = (TextView) findViewById(R.id.dagger2_text_view);
+        //使用组件进行构造，注入
+        DaggerMainActivityComponent.builder().build().inject(this);
+        Log.d(TAG, "onCreate: mTinno = " + mTinno);
+
+        dagger2TextView.setText(mTinno.toString());
+    }
+}
---
+++ app/src/main/java/com/ape/dagger2/MainActivityComponent.java
@@ -0,0 +1,16 @@
+package com.ape.dagger2;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+
+/**
+ * Created by way on 2017/9/10.
+ */
+//用这个标注标识是一个连接器
+@Component
+public interface MainActivityComponent {
+    //这个连接器要注入的对象。这个inject标注的意思是，我后面的参数对象里面有标注为@Inject的属性，这个标注的属性是需要这个连接器注入进来的。
+    void inject(MainActivity activity);
+}
